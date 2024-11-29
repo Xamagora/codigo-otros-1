2d37fdb6f0dba399fa4c5c092ec00e5de7d8fdc9
Xamagora <xaxiry_magaly@hotmail.com>
2024-11-29 11:25:24 -0600

    Agregando comentarios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,22 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('.name'); // Para seleccionar una clase en el Dom se utiliza el .seguidoDelNombre
+const $b = document.querySelector('.blog'); //El # es para los id
+const $l = document.querySelector('.location'); // No estaba en el html
 
-function displayUser(username) {
+async function displayUser(username) { //Agregando la función async.
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const response = await fetch(`${usersEndpoint}/${username}`);// Debido a que tiene un await, podemos deducir que es una  define una función asíncrona, entonces faltaría agregarle la palabra reservada async.
+  const data = await response.json();// Es necesario convertirlo a formato JSON para trabajar con los datos
+  console.log(data); //Para imprimir la respuesta
+  $n.textContent = `${data.name}`; //Al ejecutar el código me salio que estaban mal las '
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`; // se cambiaron por `backticks
 }
 
-function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
+function handleError(err) {  //Función para advertir sobre un error.
+  console.log('OH NO!'); //Imprime en consola
+  console.log(err);     
   n.textContent = `Algo salió mal: ${err}`
 }
 
